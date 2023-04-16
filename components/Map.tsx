a0a70f4ce066c6ea98b7dee7cd939823c477fd91
Brayden <dudgjs1318@gmail.com>
2023-04-15 23:59:41 -0700

    add 50vh in place.tsx and make smaller margin top

--- components/Map.tsx
+++ components/Map.tsx
@@ -13,14 +13,8 @@ type MapOptions = google.maps.MapOptions;
 const Map = () => {
   const mapRef = useRef<GoogleMap>();
   const [currentLocation, setCurrentLocation] = useState<LatLngLiteral>();
-  const [startPlace, setStartPlace] = useState<LatLngLiteral>({
-    lat: 49.2,
-    lng: -123.1,
-  });
-  const [endPlace, setEndPlace] = useState<LatLngLiteral>({
-    lat: 47.6,
-    lng: -122.3,
-  });
+  const [startPlace, setStartPlace] = useState<LatLngLiteral>();
+  const [endPlace, setEndPlace] = useState<LatLngLiteral>();
   const [directions, setDirections] = useState<DirectionsResult>();
   const [travelMode, setTravelMode] = useState<
     "DRIVING" | "WALKING" | "BICYCLING" | "TRANSIT"
