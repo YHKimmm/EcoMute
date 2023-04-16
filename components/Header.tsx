79288b14e09c80a60b45119b43fa79bfb6911fed
Mason Barry <mbarry22@my.bcit.ca>
2023-04-16 02:23:37 -0700

    Header Fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -54,7 +54,7 @@ export default function Header() {
             <div className="relative">
               <button
                 type="button"
-                className="group rounded-md text-gray-300 inline-flex items-center text-base font-medium focus:outline-none pb-8'"
+                className="group rounded-md text-gray-300 inline-flex items-center text-base font-medium hover:text-white font-semibold focus:outline-none pb-8'"
               >
                 <a className="" href="/climate">
                   Your Emissions
