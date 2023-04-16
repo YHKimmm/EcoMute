c091a7417639f5011b946e4c3f8394a4e6c90867
Mason Barry <mbarry22@my.bcit.ca>
2023-04-15 22:01:14 -0700

    Footer Links To Github

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="max-w-5xl xl:max-w-5xl mx-auto divide-y divide-gray-900 px-4 sm:px-6 md:px-8">
         <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
           <Link
-            href="/"
+            href="https://github.com/YHKimmm/EcoMute"
             className="text-md text-gray-200 hover:text-white transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold tracking-tight"
           >
             © 2023 ECOMUTE Inc.
